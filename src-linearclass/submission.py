e1707e099e0ce4acfedeea9e350069a276e02041
Sean Fitzgerald <fitzgerald.sean.t@gmail.com>
2020-11-23 22:37:13 -0500

    New assignment.

---
+++ src-linearclass/submission.py
@@ -0,0 +1,143 @@
+import numpy as np
+import util
+
+
+def main_GDA(train_path, valid_path, save_path):
+    """Problem: Gaussian discriminant analysis (GDA)
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        valid_path: Path to CSV file containing dataset for validation.
+        save_path: Path to save predicted probabilities using np.savetxt().
+    """
+    # Load dataset
+    x_train, y_train = util.load_dataset(train_path, add_intercept=False)
+
+    # *** START CODE HERE ***
+    # *** END CODE HERE ***
+
+
+class GDA:
+    """Gaussian Discriminant Analysis.
+
+    Example usage:
+        > clf = GDA()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+    def __init__(self, step_size=0.01, max_iter=10000, eps=1e-5,
+                 theta_0=None, verbose=True):
+        """
+        Args:
+            step_size: Step size for iterative solvers only.
+            max_iter: Maximum number of iterations for the solver.
+            eps: Threshold for determining convergence.
+            theta_0: Initial guess for theta. If None, use the zero vector.
+            verbose: Print loss values during training.
+        """
+        self.theta = theta_0
+        self.step_size = step_size
+        self.max_iter = max_iter
+        self.eps = eps
+        self.verbose = verbose
+
+    def fit(self, x, y):
+        """Fit a GDA model to training set given by x and y by updating
+        self.theta.
+
+        Args:
+            x: Training example inputs. Shape (n_examples, dim).
+            y: Training example labels. Shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (n_examples, dim).
+
+        Returns:
+            Outputs of shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        # *** END CODE HERE
+
+def main_LogReg(train_path, valid_path, save_path):
+    """Problem: Logistic regression with Newton's Method.
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        valid_path: Path to CSV file containing dataset for validation.
+        save_path: Path to save predicted probabilities using np.savetxt().
+    """
+    # Load dataset
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+
+    # *** START CODE HERE ***
+    # *** END CODE HERE ***
+
+
+class LogisticRegression:
+    """Logistic regression with Newton's Method as the solver.
+
+    Example usage:
+        > clf = LogisticRegression()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+    def __init__(self, step_size=0.01, max_iter=1000000, eps=1e-5,
+                 theta_0=None, verbose=True):
+        """
+        Args:
+            step_size: Step size for iterative solvers only.
+            max_iter: Maximum number of iterations for the solver.
+            eps: Threshold for determining convergence.
+            theta_0: Initial guess for theta. If None, use the zero vector.
+            verbose: Print loss values during training.
+        """
+        self.theta = theta_0
+        self.step_size = step_size
+        self.max_iter = max_iter
+        self.eps = eps
+        self.verbose = verbose
+
+    def fit(self, x, y):
+        """Run Newton's Method to minimize J(theta) for logistic regression.
+
+        Args:
+            x: Training example inputs. Shape (n_examples, dim).
+            y: Training example labels. Shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Return predicted probabilities given new inputs x.
+
+        Args:
+            x: Inputs of shape (n_examples, dim).
+
+        Returns:
+            Outputs of shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        # *** END CODE HERE ***
+
+if __name__ == '__main__':
+    main_LogReg(train_path='ds1_train.csv',
+         valid_path='ds1_valid.csv',
+         save_path='logreg_pred_1.txt')
+
+    main_LogReg(train_path='ds2_train.csv',
+         valid_path='ds2_valid.csv',
+         save_path='logreg_pred_2.txt')
+
+    main_GDA(train_path='ds1_train.csv',
+         valid_path='ds1_valid.csv',
+         save_path='gda_pred_1.txt')
+
+    main_GDA(train_path='ds2_train.csv',
+         valid_path='ds2_valid.csv',
+         save_path='gda_pred_2.txt')
